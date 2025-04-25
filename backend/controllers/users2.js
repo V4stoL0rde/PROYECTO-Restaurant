9bb4ab76fe74bee0a1e58c6fe8f83ddda4e2de42
Sebaxxxxx <111377414+sebaaaa7@users.noreply.github.com>
2025-04-24 21:52:23 -0400

    Update users2.js

--- backend/controllers/users2.js
+++ backend/controllers/users2.js
@@ -1,48 +1,221 @@
 const mongoose = require("mongoose");
+const User2 = require("../models/User2");
+const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
 
-const bcrypt = require('bcrypt');
 
-const roles = {
-    values: ['ADMIN', 'TRABAJADOR',"CLIENTE"],
-    message: '{VALUE} no es un rol v�lido'
+// Función para enviar correo electrónico
+const sendEmail = (email, subject, message) => {
+  const transporter = nodemailer.createTransport({
+   host:"smtp.gmail.com",
+   port:465,
+   secure:true,
+    auth: {
+      user: 'restaurante.ulagos@gmail.com', // Reemplaza con tu correo electrónico de Gmail
+      pass: 'tacy uycg eszo tdjj' // Reemplaza con tu contraseña de Gmail
+    }
+  });
+
+  const mailOptions = {
+    from: 'restaurante.ulagos@gmail.com', // Reemplaza con tu correo electrónico de Gmail
+    to: email,
+    subject: subject,
+    text: message
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email enviado: ' + info.response);
+    }
+  });
+};
+const email = 'restaurante.ulagos@gmail.com';
+// Función para agregar nuevo usuario
+const userVerificationCodes = {}; // Objeto para almacenar los códigos de verificación por usuario
+
+const addUser2 = (req, res) => {
+  const code = Math.floor(Math.random() * 90000) + 10000; // Genera un código de 5 dígitos
+  console.log(req.body);
+  
+  let user2 = new User2({
+    correo: req.body.correo,
+    Username: req.body.Username,
+    password: req.body.password,
+    role:req.body.role
+  });
+
+  user2.save((err, user) => {
+    if (err) {
+      res.status(500).send(err.message);
+    } else {
+      // Envía correo electrónico al nuevo usuario con el código
+
+      if (user.role[0] === 'CLIENTE') {
+        sendEmail(user.correo, 'Código de verificación para registrarse', `Tu código de verificación es ${code}. Úsalo para completar tu registro en nuestra aplicación.`);
+      }
+
+      else {
+        if(user.role[0] === 'TRABAJADOR'){
+        sendEmail(user.correo, 'Código de verificación para registrarse', `hable con el administrador para obtener su codigo de verificacion.`);
+        sendEmail(email, 'Código de verificación para registrarse', `código de verificación es ${code}.`);
+
+      }
+      } 
+      
+      
+      const date = new Date();
+      date.setHours(date.getHours() - 4);
+      userVerificationCodes[req.body.Username] = { code: code, sentTime: date ,failedAttempts: 0 };
+  
+      res.status(200).json(user);
+      
+    }
+  });
+  
+};
+
+const verifyUser2 = (req, res) => { 
+  const code = req.body.code;
+  const username = req.body.Username;
+  const date = new Date();
+  date.setHours(date.getHours() - 4); // Ajuste de zona horaria
+  
+  if (!userVerificationCodes[username]) {
+    return res.status(404).json({ message: 'Usuario no encontrado' });
   }
 
-const userSchema = new mongoose.Schema({
-  Username:   { type: String,unique: true, required : [true, 'El nombre es necesario'] },
-  password: { type: String, required: [true, 'Pass es necesario'] },
-  role: { type: [String], default: 'CLIENTE', enum: roles ,required: [true, 'Roles are mandatory']},
-  token: {type: String,required: [false, 'Session token is not mandatory'],default: '' },
-  code: { type: String },
-  correo: {type: String},
-  verified: { type: Boolean, default: false } // Nuevo campo "verified
-  
-});
-//
+  const { code: sentCode, sentTime } = userVerificationCodes[username];
 
-userSchema.pre('save', function(next) {
-    var user = this;
+  // Convierte sentCode a string porque llega como objeto
+  const sentCodeString = sentCode.toString();
+  const codeString = code.toString(); // Asegura que code sea una cadena
 
-    // only hash the password if it has been modified (or is new)
-    if (!user.isModified('password')) return next();
-
-    // generate a salt
-    bcrypt.genSalt(10, function(err, salt) {
-        if (err) return next(err);
-
-        // hash the password using our new salt
-        bcrypt.hash(user.password, salt, function(err, hash) {
-            if (err) return next(err);
-            // override the cleartext password with the hashed one
-            user.password = hash;
-            next();
-        });
+  // Verifica si el código enviado coincide con el original y si no ha pasado demasiado tiempo
+  if (sentCodeString === codeString && (date - sentTime) <= 600000) {
+    // Verificación exitosa, actualizar el usuario como verificado
+    User2.findOneAndUpdate({ Username: username }, { verified: true }, { new: true }, (err, user) => {
+      if (err) {
+        return res.status(500).send(err.message);
+      } else {
+        return res.status(200).json(user);
+      }
     });
-});
+  } else {
+    console.log('code:', code, 'sentCode:', sentCode);
 
-//coparar contrase�a encyptada
+    // Incrementa el contador de intentos fallidos
+    userVerificationCodes[username].failedAttempts = (userVerificationCodes[username].failedAttempts || 0) + 1;
 
-userSchema.methods.comparePassword = function(password) {
-    return bcrypt.compareSync(password, this.password)
-}
+    // Si el número de intentos fallidos es mayor  3, eliminar usuario
+    if (userVerificationCodes[username].failedAttempts > 3) {
+      // Eliminar al usuario de la base de datos
+      User2.deleteOne({ Username: username }, (err) => {
+        if (err) {
+          return res.status(500).send(err.message);
+        } else {
+          // Eliminar los intentos fallidos del objeto en memoria
+          delete userVerificationCodes[username];
+          return res.status(200).json({ message: 'Usuario eliminado correctamente debido a intentos fallidos de verificación.' });
+        }
+      });
+    } else {
+      // Notificar al usuario que el código es incorrecto
+      res.status(401).json({ message: 'Código de verificación inválido. Intentos fallidos: ' + userVerificationCodes[username].failedAttempts });
+    }
+  }
+};
+// Función para autenticar usuario y crear token
+const autUser2 = (req, res) => {
+  console.log(req.body)
+  User2.findOne({Username: req.body.Username})
+    .then((user => {
+      if (!user || !user.comparePassword(req.body.password)) {
+        return res.status(401).json({ message: 'Usuario o contraseña erróneos' });
+      }
 
-module.exports = User2 = mongoose.model('User2', userSchema);
+      if (!user.verified) {
+        return res.status(401).json({ message: 'Usuario no verificado' });
+      }
+
+      const token = jwt.sign({
+        Username: user.Username, user_id: user._id, role: user.role,
+      }, 'secret',process.env.JWT_KEY, { expiresIn: "7d"}) // Expira en 7 días
+      user.token = token;
+      // Envía correo electrónico al usuario autenticado
+      sendEmail(user.correo, 'Iniciaste sesión en nuestra aplicación', '¡Hola! Has iniciado sesión en nuestra aplicación. ¡Disfrútala!');
+      return res.status(200).json({
+        Username: user.Username, user_id: user._id, role: user.role,
+        token: token,
+        message:"ok"
+      })
+    }))
+    .catch ((error) => {
+      console.log(error) 
+    });
+};
+
+
+const RecuperarCuenta = (req, res) => {
+  const code = Math.floor(Math.random() * 90000) + 10000; // Genera un código de 5 dígitos
+  console.log(req.body);
+  const Username = req.body.Username;
+ 
+
+  User2.findOne({Username: Username})
+  .then((user) => {
+    if (!user) {
+      return res.status(404).json({ message: 'Usuario no encontrado' });
+    }
+
+    // Envía correo electrónico al usuario con el código de verificación
+    
+    sendEmail(user.correo, 'Código de verificación para recuperar', `Tu código de verificación es ${code}. Úsalo para recuperar tu cuenta en nuestra aplicación.`);
+    userVerificationCodes[req.body.Username] = { code: code };
+    res.status(200).json(user);
+  })
+  .catch((error) => {
+    console.log(error);
+    return res.status(500).json({ message: 'Error al buscar usuario' });
+  });
+};
+
+const RecuperarCuentaCodigo = (req, res) => {
+  const code = req.body.code;
+  const Username = req.body.Username;
+
+  if (!userVerificationCodes[Username] || toString(userVerificationCodes[Username].code) !== toString(code)) {
+    return res.status(401).json({ message: 'Código de verificación inválido' });
+  }
+
+  User2.findOneAndUpdate({Username: Username}, {password: req.body.password}, {new: true})
+  .then((user) => {
+    if (!user) {
+      return res.status(404).json({ message: 'Usuario no encontrado' });
+    }
+
+    // Elimina el código de verificación del usuario
+    delete userVerificationCodes[Username];
+
+    return res.status(200).json({ message: 'Contraseña actualizada exitosamente' });
+  })
+  .catch((error) => {
+    console.log(error);
+    return res.status(500).json({ message: 'Error al actualizar la contraseña' });
+  });
+};
+
+/* GET users listing. */
+const verUser2 = (req, res, next) => {
+  res.send('respond with a resource');
+};
+
+module.exports = {
+  addUser2,
+  autUser2,
+  verUser2,
+  verifyUser2,
+  RecuperarCuenta,
+  RecuperarCuentaCodigo
+};
